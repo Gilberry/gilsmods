2cc8ab33dc412497f0f16551e000498223728863
Gilberry <gilbert.m.allen@protonmail.com>
2026-02-21 13:38:53 +0100

    [tintable-nuclear-fuel] Initial commit

---
+++ mods/tintable-nuclear-fuel/data-updates.lua
@@ -0,0 +1,9 @@
+local plutonium_tint = {
+    primary = { r = 1.0, g = 0.4, b = 0.0, a = 1.0 },
+    secondary = { r = 1.0, g = 0.4, b = 0.0, a = 1.0 },
+    tertiary = { r = 1.0, g = 0.4, b = 0.0, a = 1.0 },
+    quaternary = { r = 1.0, g = 0.4, b = 0.0, a = 1.0 }
+}
+
+data.raw["recipe"]["breeder-fuel-reprocessing"].crafting_machine_tint = plutonium_tint
+data.raw["recipe"]["nuclear-fuel-pu"].crafting_machine_tint = plutonium_tint
